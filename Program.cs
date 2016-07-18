07d6ae4ca0cd0295e47e2c2bdfbf85825ed004bb
azziliz <chriskang@free.fr>
2016-07-18 12:02:47 +0200

    initial

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vault
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s_passPhrase = StringCipher.GeneratePassPhrase("PlayerData");
+
+            string saveFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "My Games", "Fallout Shelter");
+            string filePath = Path.Combine(saveFolder, "Vault" + (1).ToString() + ".sav");
+            string vault = File.ReadAllText(filePath);
+
+            string decrypted = StringCipher.Decrypt(vault, s_passPhrase);
+        }
+    }
+}
---
+++ StringCipher.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Vault
+{
+    public static class StringCipher
+    {
+        private const string _initVector = "tu89geji340t89u2";
+        private const int _keySize = 256;
+        private const int _passPhraseLength = 8;
+
+        public static string Encrypt(string plainText, string passPhrase)
+        {
+            byte[] bytes1 = Encoding.UTF8.GetBytes("tu89geji340t89u2");
+            byte[] bytes2 = Encoding.UTF8.GetBytes(plainText);
+            byte[] bytes3 = new Rfc2898DeriveBytes(passPhrase, bytes1).GetBytes(32);
+            RijndaelManaged rijndaelManaged = new RijndaelManaged();
+            rijndaelManaged.Mode = CipherMode.CBC;
+            ICryptoTransform encryptor = rijndaelManaged.CreateEncryptor(bytes3, bytes1);
+            MemoryStream memoryStream = new MemoryStream();
+            CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write);
+            cryptoStream.Write(bytes2, 0, bytes2.Length);
+            cryptoStream.FlushFinalBlock();
+            byte[] inArray = memoryStream.ToArray();
+            memoryStream.Close();
+            cryptoStream.Close();
+            return Convert.ToBase64String(inArray);
+        }
+
+        public static string Decrypt(string cipherText, string passPhrase)
+        {
+            byte[] bytes1 = Encoding.ASCII.GetBytes("tu89geji340t89u2");
+            byte[] buffer = Convert.FromBase64String(cipherText);
+            byte[] bytes2 = new Rfc2898DeriveBytes(passPhrase, Encoding.ASCII.GetBytes("tu89geji340t89u2")).GetBytes(32);
+            RijndaelManaged rijndaelManaged = new RijndaelManaged();
+            rijndaelManaged.Mode = CipherMode.CBC;
+            ICryptoTransform decryptor = rijndaelManaged.CreateDecryptor(bytes2, bytes1);
+            MemoryStream memoryStream = new MemoryStream(buffer);
+            CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read);
+            byte[] numArray = new byte[buffer.Length];
+            int count = cryptoStream.Read(numArray, 0, numArray.Length);
+            memoryStream.Close();
+            cryptoStream.Close();
+            return Encoding.UTF8.GetString(numArray, 0, count);
+        }
+
+        public static string GeneratePassPhrase(string plainText)
+        {
+            string plainText1 = plainText;
+            while (plainText1.Length < 8)
+                plainText1 = plainText1 + plainText;
+            return StringCipher.Base64Encode(plainText1).Substring(0, 8);
+        }
+
+        public static string Base64Encode(string plainText)
+        {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(plainText));
+        }
+
+        public static string Base64Decode(string encodedText)
+        {
+            return Encoding.UTF8.GetString(Convert.FromBase64String(encodedText));
+        }
+    }
+}
